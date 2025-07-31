e4dbef43d683c0a512b5a564a02c5ce4b969f52f
João Vicente <joao.vicente@zedia.com.br>
2025-07-31 17:28:04 -0300

    refactor: change grid size, font size and apply margin

--- src/ui/widgets/BigDay.lua
+++ src/ui/widgets/BigDay.lua
@@ -1,18 +1,17 @@
 local BigDay = {}
 
 function BigDay.load(std, data)
-    local grid = std.ui.grid('6x3')
+    local grid = std.ui.grid('3x3')
         :dir(1)
-        :add(std.node.load('src/ui/elements/TextSimple.lua'), 6)
-        :add({}, 6)
+        :add(std.node.load('src/ui/elements/TextSimple.lua'), 3)
+        :add({}, 3)
         :add(std.node.load('src/ui/elements/TextSimple.lua'))
         :add(std.node.load('src/ui/elements/TextSimple.lua'))
         :add(std.node.load('src/ui/elements/TextSimple.lua'))
-        :apply()
     
     local index = 1
     local order = {1, 3, 4, 5}
-    local hem = data.height/2
+    local hem = data.height/4
 
     while index <= #order do
         local node = grid:get_item(order[index])
@@ -24,12 +23,13 @@ function BigDay.load(std, data)
         node.data.label = data.labels and data.labels[index]
         node.data.listen = data.listeners and data.listeners[index]
         node.data.right_align = data.right_align
+        node.data.center_y = data.center_y
+        node.data.center_x = data.center_x
         index = index + 1
     end
 
-    grid:get_item(1).data.size = 100
-    grid:get_item(1).data.padding_x = data.int_padding or 0
-    grid:get_item(1).data.padding_y = -10
+    grid:get_item(1).data.size = 90
+    grid:get_item(1).data.right_align = false
 end
 
 return BigDay
